926edefcfbdb9c375861e9b54a8874752d89aebe
Boris Ermachenko <cbkf12.ru@yandex.ru>
2018-11-18 23:49:34 +0300

    lab2.0

--- 1608/ermachenko_ba/Task2.cpp
+++ 1608/ermachenko_ba/Task2.cpp
@@ -9,9 +9,6 @@
 #include <math.h>
 #include <mpi.h>
 
-#define displacement(type, member) \
-	  (((unsigned long) &((type *)0)->member) - (unsigned long)((type *)0))
-
 double log2(double x);
 int create_hypercube_topology(MPI_Comm old_comm, int n, MPI_Comm *new_comm);
 
@@ -20,42 +17,34 @@ double *tmp;
 int main(int argc, char *argv[])
 {
 	int Procnam = 0;
-	int Procrang = 0;
+	int rang = 0;
 	int cw_size;
 	int rank;
-
-	//int ntri=1;
-	//size_t buff_size = ntri * 9 * sizeof(double);
-	//double *buffer = (double*)malloc(buff_size);
+	int mess = 0;
 
 	MPI_Comm hc_comm;
-
+	MPI_Status Status;
 	MPI_Init(&argc, &argv);
 	MPI_Comm_size(MPI_COMM_WORLD, &Procnam);
-	MPI_Status Status;
+
 	int hyper_dims = log2(Procnam);
 
 	create_hypercube_topology(MPI_COMM_WORLD, hyper_dims, &hc_comm);
-	MPI_Comm_rank(hc_comm, &Procrang);
+
+	MPI_Comm_rank(hc_comm, &rank);
 
 	int i;
-	int source, dest;
+
+	int source = 0, dest = 0;
 	for (i = 0; i < hyper_dims; i++) {
 		MPI_Cart_shift(hc_comm, i, 1, &source, &dest);
-		printf("Step %2d: I am process %3d and I will excange my buffer with %3d (source = %3d)\n", i, Procrang, dest, source);
-		//printf("Example for this\n");
-		//int bufEx = ;
-		//if (Procrang == dest)
-		//MPI_Send(&Procrang, 1, MPI_INT, dest, 0, hc_comm);
-
-		//int RecvRank = 0;
-		//MPI_Recv(&RecvRank, 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, &hc_comm, &Status);
-		//printf("\n Hello from process %3d", RecvRank);
+		printf("Step %2d: I am process %3d and I will excange my buffer with %3d (source = %3d)\n", i, rank, dest, source);
+		printf("___And I have present from %3d\n", mess);
 		MPI_Barrier(hc_comm);
+		int sendr = rank;
+		MPI_Sendrecv(&sendr, 1, MPI_INT, source, 1, &mess, 1, MPI_INT, rank, 1, hc_comm, &Status);
 	}
-
 	MPI_Finalize();
-
 	return 0;
 }
 
@@ -66,13 +55,12 @@ double log2(double x)
 
 int create_hypercube_topology(MPI_Comm old_comm, const int n, MPI_Comm *new_comm)
 {
-	int i=0;
-	int *dims=(int*)malloc(n);
+	int i = 0;
+	int *dims = (int*)malloc(n);
 	int *period = (int*)malloc(n);
 	for (i = 0; i < n; ++i) {
 		dims[i] = 2;
 		period[i] = 1;
 	}
 	int err = MPI_Cart_create(old_comm, n, dims, period, 0, new_comm);
-	return err;
-}
+	re
